20ad71948cfbe4d2f4cd21598fb8328cdfd0be10
Travis Herzog <travisherzog@hds-log.com>
2017-02-13 22:01:53 -0600

    Removed @JsonIgnoreProperties class attribute

--- src/com/whiteybot/TwitchAPI/models/SearchResult.java
+++ src/com/whiteybot/TwitchAPI/models/SearchResult.java
@@ -1,6 +1,5 @@
 package com.whiteybot.TwitchAPI.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
--- src/com/whiteybot/TwitchAPI/models/TopGame.java
+++ src/com/whiteybot/TwitchAPI/models/TopGame.java
@@ -1,11 +1,8 @@
 package com.whiteybot.TwitchAPI.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 /**
  * Created by Travis on 2/12/2017.
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class TopGame {
 
     private Game game;
